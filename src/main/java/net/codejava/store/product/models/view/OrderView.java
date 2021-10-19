7cff7fb77262602e09140ca5d8209ea6b5cc34ca
Phong <phamvietphong3001@gmail.com>
2021-10-19 21:54:24 +0700

    yeahhhhh

--- src/main/java/net/codejava/store/product/models/view/OrderView.java
+++ src/main/java/net/codejava/store/product/models/view/OrderView.java
@@ -1,17 +1,17 @@
 package net.codejava.store.product.models.view;
 
 public class OrderView {
-    private String id;
+    private int id;
     private String customerName;
     private String phone;
     private String address;
     private double total;
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
