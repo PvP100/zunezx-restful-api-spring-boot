a697531bc5c6d6d40ea6155d92ab1962bcaa1583
nhantt <phong.pv@beetechsoft.vn>
2022-03-21 17:21:04 +0700

    test

--- src/main/java/net/codejava/store/product/models/view/CategoryView.java
+++ src/main/java/net/codejava/store/product/models/view/CategoryView.java
@@ -5,7 +5,6 @@ import net.codejava.store.product.models.data.Category;
 public class CategoryView {
     private String id;
     private String title;
-    private int quantity;
 
     public CategoryView() {
     }
@@ -13,7 +12,6 @@ public class CategoryView {
     public CategoryView(Category category) {
         this.id = category.getId();
         this.title = category.getTitle();
-        this.quantity = 0;
     }
 
     public String getId() {
@@ -30,13 +28,5 @@ public class CategoryView {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 }
