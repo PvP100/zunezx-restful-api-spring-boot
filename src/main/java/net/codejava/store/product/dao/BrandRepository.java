cecfd72366c4fb900b00c62aa1029800f0b8d02d
nhantt <phong.pv@beetechsoft.vn>
2022-04-12 17:58:50 +0700

    heroku

--- src/main/java/net/codejava/store/product/dao/BrandRepository.java
+++ src/main/java/net/codejava/store/product/dao/BrandRepository.java
@@ -2,6 +2,8 @@ package net.codejava.store.product.dao;
 
 import net.codejava.store.product.models.data.Brand;
 import net.codejava.store.product.models.view.BrandView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +15,7 @@ public interface BrandRepository extends JpaRepository<Brand, Integer> {
             " from Brand b ")
     List<BrandView> getBrand();
 
+    @Query("select new net.codejava.store.product.models.view.BrandView(b) " +
+            "from Brand b where b.brandName like %?1%")
+    Page<BrandView> searchBrandByName(Pageable pageable, String name);
 }
--- src/main/java/net/codejava/store/product/dao/CategoryRepository.java
+++ src/main/java/net/codejava/store/product/dao/CategoryRepository.java
@@ -2,6 +2,8 @@ package net.codejava.store.product.dao;
 
 import net.codejava.store.product.models.data.Category;
 import net.codejava.store.product.models.view.CategoryView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,6 +18,10 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     @Query("select new net.codejava.store.product.models.view.CategoryView(c) " +
             " from Category c ")
     List<CategoryView> getCategoryCount();
+
+    @Query("select new net.codejava.store.product.models.view.CategoryView(c) " +
+            "from Category c where c.title like %?1%")
+    Page<CategoryView> searchCategoryByName(Pageable pageable, String name);
 
 //    @Query("select new net.codejava.store.product.models.view.CategoryView(c) " +
 //            " from Category c where c.categoryType = ?1")
